270923d74f3ed8abe1d514db8af58fcebbb75d2c
zk2487 <45111205+zk2487@users.noreply.github.com>
2019-07-30 17:09:59 -0500

    Initial commit

---
+++ number_reader.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# Reshaping the array to 4-dims so that it can work with the Keras API
+input_shape = (28, 28, 1)
+img_rows, img_cols = input_shape[0],input_shape[1]
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+# Making sure that the values are float so that we can get decimal points after division
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalizing the RGB codes by dividing it to the max RGB value.
+
+x_train /= 255
+x_test /= 255
+
+print('x_train shape:', x_train.shape)
+print('Number of images in x_train', x_train.shape[0])
+print('Number of images in x_test', x_test.shape[0])
+
+# Importing the required Keras modules containing model and layers
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+
+# Creating a Sequential Model and adding the layers
+def CNN_model():
+    model = Sequential()
+    model.add(Conv2D(28, kernel_size=(3,3), input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten()) # Flattening the 2D arrays for fully connected layers
+    model.add(Dense(128, activation=tf.nn.relu))
+    model.add(Dropout(0.2))
+    model.add(Dense(10,activation=tf.nn.softmax))
+    return model
+model = CNN_model()
+
+# Compile and fitting the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x=x_train,y=y_train, epochs=3)
+model.evaluate(x_test, y_test)
+model.summary()
+
+# Test the CNN
+import matplotlib.pyplot as plt
+image_index = 67 # image you want to get access
+plt.imshow(x_test[image_index].reshape(28, 28),cmap='Greys')
+plt.show()
+pred = model.predict(x_test[image_index].reshape(1, img_rows, img_cols, 1))
+print(pred.argmax())
